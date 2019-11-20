4678f8edcc35bb5a4278c2c4c0888ced2005b6d0
dl8sd11 <tmd910607@gmail.com>
2019-11-20 22:57:41 +0800

    remain the part I really type, also fixed debug

--- MISC/Template.cpp
+++ MISC/Template.cpp
@@ -1,89 +1,47 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-typedef pair<int, ll> pil;
-typedef pair<ll, int> pli;
-typedef pair<double,double> pdd;
-#define SQ(i) ((i)*(i))
 #define MEM(a, b) memset(a, (b), sizeof(a))
 #define SZ(i) int(i.size())
-#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
-#define FOR3(i, a, b) for (int i = (a); i<(b); i++)
-#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
-#define REP(i, j) FOR(i, 0, j, 1)
-#define REP1(i,j) FOR(i, 1, j+1, 1)
-#define RREP(i, j) RFOR(i, j, 0, 1)
+#define REP(i, j) for(int i=0;i<(j);++i)
+#define REP1(i,j) for(int i=1;i<=(j);++i)
+#define RREP(i, j) for(int i=j;i>=0;i--)
 #define ALL(_a) _a.begin(),_a.end()
-#define mp make_pair
 #define pb push_back
 #define eb emplace_back
 #define X first
 #define Y second
-#define f first
-#define s second
 #define MN(a,b) a = min(a,(__typeof__(a))(b))
 #define MX(a,b) a = max(a,(__typeof__(a))(b))
-#define SORT_UNIQUE(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
 #ifdef BTC
-#define TIME(i) Timer i(#i)
-#define debug(...) do{\
-    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
-    _do(__VA_ARGS__);\
-}while(0)
-template<typename T>void _do(T &&_x){cerr<<_x<<endl;}
-template<typename T,typename ...S> void _do(T &&_x,S &&..._t){cerr<<_x<<" ,";_do(_t...);}
-template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p){return _s<<"("<<_p.X<<","<<_p.Y<<")";}
-template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb)
+#define debug(...) fprintf(stderr,"#%d: %s = ",__LINE__,#__VA_ARGS__),_do(__VA_ARGS__);
+template<typename T> void _do(T &&x){cerr<<x<<endl;}
+template<typename T, typename ...S> void _do(T &&x, S &&...y){cerr<<x<<", ";_do(y...);}
+template<typename It> ostream& _printRng(ostream &os,It bg,It ed)
 {
-    _s<<"{";
-    for(It _it=_ita;_it!=_itb;_it++)
-    {
-        _s<<(_it==_ita?"":",")<<*_it;
+    os<<"{";
+    for(It it=bg;it!=ed;it++) {
+        os<<(it==bg?"":",")<<*it;
     }
-    _s<<"}";
-    return _s;
+    os<<"}";
+    return os;
 }
-template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c){return _OUTC(_s,ALL(_c));}
-template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){return _OUTC(_s,ALL(_c));}
-template<typename _a> ostream &operator << (ostream &_s,deque<_a> &_c){return _OUTC(_s,ALL(_c));}
-template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c){return _OUTC(_s,ALL(_c));}
-template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
+template<typename T> ostream &operator << (ostream &os,vector<T> &v){return _printRng(os,v.begin(), v.end());}
+template<typename T> void pary(T bg, T ed){_printRng(cerr,bg,ed);cerr<<endl;}
 #define IOS()
-class Timer {
-private:
-    string scope_name;
-    chrono::high_resolution_clock::time_point start_time;
-public:
-    Timer (string name) : scope_name(name) {
-        start_time = chrono::high_resolution_clock::now();
-    }
-    ~Timer () {
-        auto stop_time = chrono::high_resolution_clock::now();
-        auto length = chrono::duration_cast<chrono::microseconds>(stop_time - start_time).count();
-        double mlength = double(length) * 0.001;
-        debug(scope_name, mlength);
-    }
-};
 #else
-#define TIME(i)
 #define debug(...)
 #define pary(...)
 #define endl '\n'
 #define IOS() ios_base::sync_with_stdio(0);cin.tie(0)
 #endif
 
-const ll MOD = 1000000007;
 const ll INF = 0x3f3f3f3f3f3f3f3f;
-const int iNF = 0x3f3f3f3f;
-// const int MAXN = 
-
-void GG(){cout<<"-1\n"; exit(0);}
 
 /********** Good Luck :) **********/
 int main () {
-    TIME(main);
     IOS();
 
     return 0;
