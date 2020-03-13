28bd2b4180670943052815b15c90b607a85ec816
le9018468 <elliotliu123@gmail.com>
2020-03-13 22:51:07 +0800

    OAO
    Hi TMD

--- DataStructure/ConvexHull.cpp
+++ DataStructure/ConvexHull.cpp
@@ -1,20 +1,22 @@
+
+// Lower Hull
 bool QTYPE=0;
 struct Line {
     mutable ll k, m, p;
-    bool operator<(const Line& o) const { 
+    bool operator<(const Line& o) const {
         if (QTYPE) return p<o.p;
-        return k < o.k; 
+        return k < o.k;
     }
 };
 
 struct LineContainer : multiset<Line > {
-    // (for doubles, use inf = 1/.0, div(a,b) = a/b)
-    const ll inf = LLONG_MAX;
+    // (for doubles, use INF = 1/.0, div(a,b) = a/b)
+    const ll INF = LLONG_MAX;
     ll div(ll a, ll b) { // floored division
         return a / b - ((a ^ b) < 0 && a % b); }
     bool isect(iterator x, iterator y) {
-        if (y == end()) { x->p = inf; return false; }
-        if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+        if (y == end()) { x->p = INF; return false; }
+        if (x->k == y->k) x->p = x->m > y->m ? INF : -INF;
         else x->p = div(y->m - x->m, x->k - y->k);
         return x->p >= y->p;
     }
