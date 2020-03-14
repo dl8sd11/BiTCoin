ec17275d9438aa92a8f53a477e05b893eba85d53
le9018468 <elliotliu123@gmail.com>
2020-03-14 22:43:12 +0800

    Update ConvexHull.cpp

--- DataStructure/ConvexHull.cpp
+++ DataStructure/ConvexHull.cpp
@@ -2,26 +2,26 @@
 // Lower Hull
 bool QTYPE=0;
 struct Line {
-    mutable ll k, m, p;
+    mutable ll m, b, p;
     bool operator<(const Line& o) const {
         if (QTYPE) return p<o.p;
-        return k < o.k;
+        return m < o.m;
     }
 };
 
 struct LineContainer : multiset<Line > {
     // (for doubles, use INF = 1/.0, div(a,b) = a/b)
     const ll INF = LLONG_MAX;
-    ll div(ll a, ll b) { // floored division
-        return a / b - ((a ^ b) < 0 && a % b); }
+    ll div(ll A, ll B) { // floored division
+        return A / B - ((A ^ B) < 0 && A % B); }
     bool isect(iterator x, iterator y) {
         if (y == end()) { x->p = INF; return false; }
-        if (x->k == y->k) x->p = x->m > y->m ? INF : -INF;
-        else x->p = div(y->m - x->m, x->k - y->k);
+        if (x->m == y->m) x->p = x->b > y->b ? INF : -INF;
+        else x->p = div(y->b - x->b, x->m - y->m);
         return x->p >= y->p;
     }
-    void add(ll k, ll m) {
-        auto z = insert({k, m, 0}), y = z++, x = y;
+    void add(ll m, ll b) {
+        auto z = insert({m, b, 0}), y = z++, x = y;
         while (isect(y, z)) z = erase(z);
         if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
         while ((y = x) != begin() && (--x)->p >= y->p)
@@ -30,6 +30,6 @@ struct LineContainer : multiset<Line > {
     ll query(ll x) {
         assert(!empty());
         QTYPE=1; auto l = *lower_bound({0,0,x}); QTYPE = 0;
-        return l.k * x + l.m;
+        return l.m * x + l.b;
     }
 };
